b89b40857ecd21556f2765e49645a734e6b74689
TokuCode <salvadorcreativo429@gmail.com>
2025-08-23 21:21:28 -0500

    Singleton helper
    - Helpers folder creater
    - Singleton script created

---
+++ Assets/Code/Helpers/Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Code.Helpers.Singleton
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        protected static T instance;
+        
+        public static bool HasInstance => instance != null;
+        public static T TryGetInstance() => HasInstance ? instance : null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType(typeof(T)) as T;
+                }
+                return instance;
+            }
+        }
+
+        protected void Awake()
+        {
+            InitializeSingleton();
+        }
+
+        protected virtual void InitializeSingleton()
+        {
+            if(!Application.isPlaying) return;
+            
+            instance = this as T;
+        }
+    }
+}
